b29e6f44e362f2ee4de3c5f822f4b5a4f2176994
trotha01 <trevor.rothaus@sendgrid.com>
2018-10-18 20:59:53 -0700

    update transform example to 0.19

--- examples/transform/Transform.elm
+++ examples/transform/Transform.elm
@@ -1,24 +1,31 @@
 module Transform exposing (main)
 
-import Html.App exposing (program)
+import BoxesAndBubbles exposing (..)
 import BoxesAndBubbles.Body as Body exposing (..)
 import BoxesAndBubbles.Engine as Engine
-import BoxesAndBubbles exposing (..)
 import BoxesAndBubbles.Math2D exposing (mul2)
-import List exposing (map)
+import Browser exposing (..)
+import Browser.Dom exposing (..)
+import Browser.Events exposing (..)
 import Collage exposing (..)
-import Element exposing (..)
+import Collage.Layout exposing (..)
+import Collage.Render exposing (..)
+import Collage.Text exposing (fromString)
 import Color exposing (..)
-import AnimationFrame
-import Time exposing (Time)
-import Keyboard.Extra as Keyboard
+import Keyboard
+import Keyboard.Arrows as Keyboard
+import List exposing (map)
+
 
 
 -- MODEL
 
 
 type alias Model meta =
-    List (Body meta)
+    { user : Body meta
+    , bodies : List (Body meta)
+    , keys : List Keyboard.Key
+    }
 
 
 type alias Meta =
@@ -29,13 +36,19 @@ type alias Meta =
 {- meta is used to tell if the body has been eaten -}
 
 
-meta : Bool
-meta =
+bodyMeta : Bool
+bodyMeta =
     False
 
 
-( height, width ) =
-    ( 600, 600 )
+height =
+    600
+
+
+width =
+    600
+
+
 bColor : Color
 bColor =
     yellow
@@ -48,44 +61,41 @@ boxColor =
 
 someBodies : List (Body Meta)
 someBodies =
-    [ bubble bColor 20 1 e0 ( -80, 0 ) ( -1.5, 0 ) meta
-    , bubble bColor 15 1 e0 ( 0, 200 ) ( -0.4, 1.0 ) meta
-    , bubble bColor 5 1 e0 ( 200, -200 ) ( -1, -1 ) meta
-    , bubble bColor 15 5 0.4 ( 100, 100 ) ( 1, 1 ) meta
-    , bubble bColor 10 1 e0 ( 200, 200 ) ( 1, -1 ) meta
-    , box boxColor ( 10, 10 ) 1 e0 ( 200, 0 ) ( 0, 0 ) meta
-    , box boxColor ( 20, 20 ) 1 e0 ( -200, 0 ) ( 3, 0 ) meta
-    , box boxColor ( 15, 15 ) 1 e0 ( 200, -200 ) ( -1, -1 ) meta
+    [ bubble bColor 20 1 e0 ( -80, 0 ) ( -1.5, 0 ) bodyMeta
+    , bubble bColor 15 1 e0 ( 0, 200 ) ( -0.4, 1.0 ) bodyMeta
+    , bubble bColor 5 1 e0 ( 200, -200 ) ( -1, -1 ) bodyMeta
+    , bubble bColor 15 5 0.4 ( 100, 100 ) ( 1, 1 ) bodyMeta
+    , bubble bColor 10 1 e0 ( 200, 200 ) ( 1, -1 ) bodyMeta
+    , box boxColor ( 10, 10 ) 1 e0 ( 200, 0 ) ( 0, 0 ) bodyMeta
+    , box boxColor ( 20, 20 ) 1 e0 ( -200, 0 ) ( 3, 0 ) bodyMeta
+    , box boxColor ( 15, 15 ) 1 e0 ( 200, -200 ) ( -1, -1 ) bodyMeta
     ]
-        ++ bounds ( width - 50, height - 50 ) 1 e0 ( 0, 0 ) meta
+        ++ bounds ( width - 50, height - 50 ) 1 e0 ( 0, 0 ) bodyMeta
 
 
-user : Body Meta
-user =
-    bubble blue 100 1 e0 ( -80, 0 ) ( 0, 0 ) meta
+initialUser : Body Meta
+initialUser =
+    bubble blue 100 1 e0 ( -80, 0 ) ( 0, 0 ) bodyMeta
 
 
 
 -- VIEW
 
 
-scene : ( Body meta, Model meta, Keyboard.Model ) -> Element
-scene ( user, bodies, keyboard ) =
-    collage width height <| map drawBody (user :: bodies)
+scene : Model meta -> Collage msg
+scene { user, bodies, keys } =
+    group <| map drawBody (bodies ++ [ user ])
 
 
-drawBody : Body meta -> Form
+drawBody : Body meta -> Collage msg
 drawBody { color, pos, velocity, inverseMass, restitution, shape, meta } =
     let
-        veloLine =
-            segment ( 0, 0 ) (mul2 velocity 5) |> traced (solid red)
-
         ready =
             case shape of
                 Bubble radius ->
                     group
                         [ circle radius
-                            |> filled color
+                            |> filled (uniform color)
                         ]
 
                 Box extents ->
@@ -93,11 +103,11 @@ drawBody { color, pos, velocity, inverseMass, restitution, shape, meta } =
                         ( w, h ) =
                             extents
                     in
-                        group
-                            [ rect (w * 2) (h * 2) |> filled color
-                            ]
+                    group
+                        [ rectangle (w * 2) (h * 2) |> filled (uniform color)
+                        ]
     in
-        Collage.move pos ready
+    Collage.shift pos ready
 
 
 
@@ -105,41 +115,44 @@ drawBody { color, pos, velocity, inverseMass, restitution, shape, meta } =
 
 
 type Msg
-    = Tick Time
+    = Tick Float
     | KeyPress Keyboard.Msg
 
 
-update : Msg -> ( Body Meta, Model Meta, Keyboard.Model ) -> ( ( Body Meta, Model Meta, Keyboard.Model ), Cmd Msg )
-update msg ( user, bodies, keyboard ) =
+update : Msg -> Model Meta -> ( Model Meta, Cmd Msg )
+update msg { user, bodies, keys } =
     case msg of
         Tick dt ->
             let
                 ( collidedUser, userCollidedBodies ) =
                     collideUser user bodies
 
+                ( gravity, ambiant ) =
+                    noGravity dt
+
                 newUser =
-                    (uncurry Engine.update (noGravity dt)) collidedUser
+                    Engine.update gravity ambiant collidedUser
 
                 collidedBodies =
                     collideBodies dt userCollidedBodies
             in
-                ( ( newUser, (collidedBodies), keyboard ), Cmd.none )
+            ( { user = newUser, bodies = collidedBodies, keys = keys }, Cmd.none )
 
         KeyPress keyMsg ->
             let
-                ( kybrd, keyboardCmd ) =
-                    Keyboard.update keyMsg keyboard
+                pressedKeys =
+                    Keyboard.update keyMsg keys
 
                 direction =
-                    Keyboard.arrows kybrd
+                    Keyboard.arrows pressedKeys
 
                 updatedUser =
                     Body.move user direction
             in
-                ( ( updatedUser, bodies, keyboard ), Cmd.map KeyPress keyboardCmd )
+            ( { user = updatedUser, bodies = bodies, keys = pressedKeys }, Cmd.none )
 
 
-collideUser : Body Meta -> Model Meta -> ( Body Meta, Model Meta )
+collideUser : Body Meta -> List (Body Meta) -> ( Body Meta, List (Body Meta) )
 collideUser user bodies =
     List.foldl
         (\b ( u, bs ) ->
@@ -147,7 +160,7 @@ collideUser user bodies =
                 ( u2, b2 ) =
                     collide u b
             in
-                ( u2, b2 :: bs )
+            ( u2, b2 :: bs )
         )
         ( user, [] )
         bodies
@@ -169,15 +182,17 @@ collide a0 b0 =
                     Bubble r ->
                         if collisionResult.penetration > r * 2 || collisionResult.penetration < r then
                             swallow a0 b0
+
                         else
-                            (Engine.resolveCollision collisionResult a0 b0)
+                            Engine.resolveCollision collisionResult a0 b0
 
                     Box _ ->
                         Engine.resolveCollision collisionResult a0 b0
+
             else
                 Engine.resolveCollision collisionResult a0 b0
     in
-        ( a1, b1 )
+    ( a1, b1 )
 
 
 swallow : Body Meta -> Body Meta -> ( Body Meta, Body Meta )
@@ -206,14 +221,14 @@ combineShapes a0 b0 =
                         boxSide =
                             sqrt ((a1 + a2) / 2)
                     in
-                        { a0 | shape = Box ( boxSide, boxSide ), color = boxColor }
+                    { a0 | shape = Box ( boxSide, boxSide ), color = boxColor }
 
                 _ ->
                     { a0 | shape = Box ( 15, 15 ) }
 
         -- we should never hit this case, only circles are food
     in
-        { combined | meta = False }
+    { combined | meta = False }
 
 
 collideBodyWith : Body Meta -> List (Body Meta) -> List (Body Meta) -> List (Body Meta)
@@ -227,25 +242,26 @@ collideBodyWith a0 bodies acc =
                 collisionResult =
                     Engine.collision a0 b0
             in
-                if
-                    collisionResult.penetration
-                        > 0
-                        && a0.meta
-                        == True
-                        && b0.meta
-                        == True
-                then
-                    let
-                        combined =
-                            combineShapes a0 b0
-                    in
-                        collideBodyWith combined bs (acc)
-                else
-                    let
-                        ( a1, b1 ) =
-                            Engine.resolveCollision collisionResult a0 b0
-                    in
-                        collideBodyWith a1 bs (b1 :: acc)
+            if
+                collisionResult.penetration
+                    > 0
+                    && a0.meta
+                    == True
+                    && b0.meta
+                    == True
+            then
+                let
+                    combined =
+                        combineShapes a0 b0
+                in
+                collideBodyWith combined bs acc
+
+            else
+                let
+                    ( a1, b1 ) =
+                        Engine.resolveCollision collisionResult a0 b0
+                in
+                collideBodyWith a1 bs (b1 :: acc)
 
 
 collideBodiesAcc : List (Body Meta) -> List (Body Meta) -> List (Body Meta)
@@ -263,10 +279,13 @@ collideBodiesAcc acc bodies =
                     collideBodiesAcc (h1 :: acc) t1
 
 
-collideBodies : Float -> Model Meta -> Model Meta
+collideBodies : Float -> List (Body Meta) -> List (Body Meta)
 collideBodies dt bodies =
-    -- uncurry step (noGravity dt) bodies
-    List.map (uncurry Engine.update (noGravity dt)) (collideBodiesAcc [] bodies)
+    let
+        ( gravity, ambiant ) =
+            noGravity dt
+    in
+    List.map (Engine.update gravity ambiant) (collideBodiesAcc [] bodies)
 
 
 
@@ -277,7 +296,7 @@ subs : Sub Msg
 subs =
     Sub.batch
         [ Sub.map KeyPress Keyboard.subscriptions
-        , AnimationFrame.diffs Tick
+        , onAnimationFrameDelta Tick
         ]
 
 
@@ -285,18 +304,27 @@ subs =
 -- MAIN
 
 
-main : Program Never
+type alias Flags =
+    {}
+
+
+initialModel : Model Meta
+initialModel =
+    { user = initialUser, bodies = someBodies, keys = [] }
+
+
+init : Flags -> ( Model Meta, Cmd Msg )
+init flags =
+    ( initialModel, Cmd.none )
+
+
 main =
-    let
-        ( keyboard, keyboardCmd ) =
-            Keyboard.init
-    in
-        program
-            { init = ( ( user, someBodies, keyboard ), Cmd.map KeyPress keyboardCmd )
-            , update = update
-            , subscriptions = always subs
-            , view = scene >> Element.toHtml
-            }
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = always subs
+        , view = scene >> Collage.Render.svgBox ( width, height )
+        }
 
 
 
